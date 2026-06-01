bff9bb2d3905803b84377c5ce7bbd378414c9c6e
yoonmi12 <yoong12@codeidea.dev>
2026-06-01 17:25:40 +0900

    페이지추가

--- link.php
+++ link.php
@@ -115,7 +115,12 @@ echo txtRecord('./@record/');
 		</li>
 		<li data-label="카지노">
 			<ul>
-				<li><a href="./casino_live.html" target="_blank" class="">라이브 카지노</a></li>
+				<li>
+					<a href="./casino_live.html" target="_blank" class="">라이브 카지노</a>
+					<ul>
+						<li><a href="./casino_detail.html" target="_blank" class="add">게임상세</a></li>
+					</ul>
+				</li>
 				<li><a href="./casino_slot.html" target="_blank" class="">슬롯</a></li>
 				<li><a href="./casino_bet.html" target="_blank" class="">벳게임즈</a></li>
 				<li><a href="./casino_table.html" target="_blank" class="">테이블게임</a></li>
@@ -131,12 +136,28 @@ echo txtRecord('./@record/');
 				<li>
 					<a href="./user_message.html" target="_blank" class="">쪽지 - 전체</a>
 					<a href="./user_message.html?tab=1" target="_blank" class="">쪽지 - 안읽음</a>
-					<a href="./user_message.html?tab=1" target="_blank" class="">쪽지 - 읽음</a>
+					<a href="./user_message.html?tab=2" target="_blank" class="">쪽지 - 읽음</a>
 				</li>
 				<li><a href="./user_friendmng.html" target="_blank" class="">지인관리</a></li>
 				<li>
-					<a href="./user_bettinghistory.html" target="_blank" class="">베팅내역</a>
-					<a href="./user_bettinghistory_nodata.html" target="_blank" class="">베팅내역 - 데이터 없을때</a>
+					<a href="./user_bettinghistory.html" target="_blank" class="add">베팅내역</a>
+					<a href="./user_bettinghistory.html?krw" target="_blank" class="add">베팅내역 - KRW</a>
+					<a href="./user_bettinghistory.html?usdt" target="_blank" class="add">베팅내역 - USDT</a>
+					<a href="./user_bettinghistory_nodata.html" target="_blank" class="add">베팅내역 - 데이터 없을때</a>
+				</li>
+				<li>
+					<a href="./user_transactiondetail.html" target="_blank" class="add">거래내역 - 입금내역</a>
+					<a href="./user_transactiondetail.html?tab=1" target="_blank" class="add">거래내역 - 출금내역</a>
+					<a href="./user_transactiondetail.html?tab=2" target="_blank" class="add">거래내역 - 포인트 사용내역</a>
+				</li>
+				<li>
+					<a href="./user_cryptobenefit.html" target="_blank" class="add">crypto 혜택</a>
+				</li>
+				<li>
+					<a href="./user_moneylog.html" target="_blank" class="add">머니로그 : 롤링</a>
+				</li>
+				<li>
+					<a href="./setting.html" target="_blank" class="add" data-date="진행중">설정 - 회원정보</a>
 				</li>
 			</ul>
 		</li>
@@ -144,6 +165,29 @@ echo txtRecord('./@record/');
 			<ul>
 				<li><a href="./event.html" target="_blank" class="">이벤트</a></li>
 				<li><button class="pop-modal" onclick="modalOpen('event_detail-modal')">이벤트 상세 모달</button></li>
+			</ul>
+		</li>
+		<li data-label="지인추천">
+			<ul>
+				<li>
+					<a href="./user_affiliate.html" target="_blank" class="add">제휴 프로그램 - 개요</a>
+					<a href="./user_affiliate.html?tab=1" target="_blank" class="add">제휴 프로그램 - 추천한 사용자</a>
+				</li>
+			</ul>
+		</li>
+		<li data-label="우리에 대해">
+			<ul>
+				<li>
+					<a href="./aboutus.html" target="_blank" class="add">우리에 대해 - 회원정보</a>
+					<a href="./aboutus.html?tab=1" target="_blank" class="add">우리에 대해 - 자금 세탁 방지</a>
+					<a href="./aboutus.html?tab=2" target="_blank" class="add">우리에 대해 - 개인정보 보호 정책</a>
+					<a href="./aboutus.html?tab=3" target="_blank" class="add">우리에 대해 - 암호화폐 코인 가이드</a>
+				</li>
+				<li>
+					<a href="./aboutus.html?tab=4" target="_blank" class="add">우리에 대해 - 제공자 가용성 정책</a>
+					<a href="./aboutus.html?tab=5" target="_blank" class="add">우리에 대해 - 스포츠 규정 및 정책</a>
+					<a href="./aboutus.html?tab=6" target="_blank" class="add">우리에 대해 - 자체 정지 정책</a>
+				</li>
 			</ul>
 		</li>
 		
